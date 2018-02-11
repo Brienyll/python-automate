444c836ac92110694bbcd8e054a06c2f4e3b0a50
Brienyll <brienyll29@gmail.com>
2018-02-11 02:44:15 -0800

    added lists examples

---
+++ lists2.py
@@ -0,0 +1,12 @@
+for i in range(5):
+    print(i)
+
+for i in [1,2,3]:
+    print(i)
+
+spam = list(range(0, 100, 2))
+
+supplies = ['pens', 'staples', 'pencils', 'binders']
+
+for i in range(len(supplies)):
+    print('Index ' + str(i) + ' in supplies is: '+  supplies[i])
