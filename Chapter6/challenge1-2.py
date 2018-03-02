faf2a012b1501b6c058b3fd0f33e75005cbf5902
Brienyll <brienyll29@gmail.com>
2018-03-01 20:04:16 -0800

    Create challenge1-2.py

---
+++ Chapter6/challenge1-2.py
@@ -0,0 +1,14 @@
+str = "Camus"
+
+print(str[0])
+print(str[1])
+print(str[2])
+print(str[3])
+print(str[4])
+
+r1 = input('Enter response one:')
+r2 = input('Enter response two:')
+
+rep = 'Yesterday I wrote a {}. I sent it to {}'.format(r1,r2)
+
+print(rep)
