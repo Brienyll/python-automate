1b61de03105dc2e2afb315a3c0c6c7db5b01932f
Brienyll <brienyll29@gmail.com>
2018-04-09 20:56:50 -0700

    Update Map.py

--- Map.py
+++ Map.py
@@ -4,3 +4,8 @@ def add_five(x):
 nums = [11, 22, 33, 44, 55]
 result = list(map(add_five, nums))
 print(result)
+
+nums = [11, 22, 33, 44, 55]
+
+result = list(map(lambda x: x+5, nums))
+print(result)
