1e2ffd87194aac8c8a7bdf6f19a353358d2bc362
Brienyll <brienyll29@gmail.com>
2018-04-13 23:42:54 -0700

    created challenge 1

---
+++ Chapter12/challenge1.py
@@ -0,0 +1,11 @@
+class Orange:
+    def __init__(self):
+        self.weight = 5
+        self.color = "Red"
+        self.kind = "Fuji"
+        self.store = "Walmart"
+        print("Created")
+
+orange = Orange()
+
+print(orange)
