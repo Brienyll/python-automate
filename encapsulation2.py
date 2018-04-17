4cffea0747d52ef6fe0e05e082af5d0812f2729e
Brienyll <brienyll29@gmail.com>
2018-04-16 23:01:57 -0700

    added hiding data

---
+++ encapsulation2.py
@@ -0,0 +1,10 @@
+class Data:
+    def __init__(self):
+        self.nums = [1, 2, 3, 4, 5]
+
+    def change_data(self, index, n):
+        self.nums[index] = n
+    
+data = Data()
+data.nums[1] = 100
+print(data.nums)
