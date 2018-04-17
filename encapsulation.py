fc7e005ea5dd27c14c187237510b558c6143bb53
Brienyll <brienyll29@gmail.com>
2018-04-16 22:55:43 -0700

    added encapsulation example

---
+++ encapsulation.py
@@ -0,0 +1,12 @@
+
+class Rectangle():
+    def __init__(self, w, l):
+        self.width = w
+        self.len = l
+
+
+    def area(self):
+        return self.width * self.len
+
+rec = Rectangle(3,5)
+print(rec.area())
