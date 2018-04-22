254198740ce08bf21cd8fb3430b2c1e418adbceb
Brienyll <brienyll29@gmail.com>
2018-04-22 03:58:16 -0700

    updated method over riding

--- inheritMethodOverride.py
+++ inheritMethodOverride.py
@@ -8,10 +8,14 @@ class Shape():
               """.format(self.width,
                          self.len))
 
-
 class Square(Shape):
     def area(self):
         return self.width * self.len
 
+    def print_size(self):
+      print("""I am {} by {}
+            """.format(self.width,
+                       self.len))
+
 a_square = Square(20, 20)
-print(a_square.area())
+a_square.print_size()
