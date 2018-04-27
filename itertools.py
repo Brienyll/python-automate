b42cdf234d5d2af4a04400d3f07e778379b33a8f
Brix <brienyll29@gmail.com>
2018-04-26 19:39:44 -0700

    Update itertools.py

--- itertools.py
+++ itertools.py
@@ -3,3 +3,6 @@ from itertools import accumulate, takewhile
 nums = list(accumulate(range(8)))
 print(nums)
 print(list(takewhile(lambda x: x<= 6, nums)))
+
+# [0, 1, 3, 6, 10, 15, 21, 28]
+# [0, 1, 3, 6]
